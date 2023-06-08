cc8ea26c15888fd52e418e115eba5f270664483b
LeeGeumJu <prettylee620@naver.com>
2023-06-08 17:18:02 +0900

    제곱 있는 거 커밋

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def add_func(n1, n2):
 def sub_func(n1, n2):
     return n1-n2
 
+def ex_func(n1, n2):
+    return n1**n2
+
 
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
+num3, num4, res = 4, 3, 0
 
 
 ## 메인 코드부
@@ -16,3 +20,6 @@ print(num1, "+", num2, "=", res)
 
 res = sub_func(num1, num2)
 print(num1, "-", num2, "=", res)
+
+res = ex_func(num3, num4)
+print(num3, "**", num4, "=", res)
